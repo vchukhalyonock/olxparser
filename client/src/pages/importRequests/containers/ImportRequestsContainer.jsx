99bb13154eabd9b0b91c9bfc5bd910661a7284e3
Volodymyr Chukhalonok <vchukhalonok@lohika.com>
2020-01-23 11:29:21 +0200

    Add search field for IR to UI

--- client/src/pages/importRequests/containers/ImportRequestsContainer.jsx
+++ client/src/pages/importRequests/containers/ImportRequestsContainer.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import {
     Container,
     Grid,
-    makeStyles,
     Paper,
     Button,
+    OutlinedInput,
+    InputAdornment,
+    withStyles
 } from "@material-ui/core";
+import {
+    Search as SearchIcon
+} from "@material-ui/icons";
 import ImportRequestsTable from "../components/ImportRequestsTable";
 import ListItemLink from "../../../components/listItemLink";
 import { CREATE_IMPORT_REQUEST_PAGE_PATH } from "../../../constants/router";
 
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
     container: {
         paddingTop: theme.spacing(4),
         paddingBottom: theme.spacing(4),
@@ -24,34 +29,53 @@ const useStyles = makeStyles(theme => ({
     button: {
         margin: theme.spacing(1)
     },
-}));
+});
 
-export default () => {
-    const classes = useStyles();
+class ImportRequestsContainer extends Component {
 
-    return (
-        <Container maxWidth="lg" className={classes.container}>
-            <Grid container spacing={3}>
-                <Grid item xs={10}/>
-                <Grid item xs={2}>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        size="medium"
-                        className={classes.button}
-                        component={ListItemLink}
-                        to={CREATE_IMPORT_REQUEST_PAGE_PATH}
-                    >
-                        Create IR
-                    </Button>
+    onChangeSearchHandler = (e) => {
+
+    };
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <Container maxWidth="lg" className={classes.container}>
+                <Grid container spacing={3}>
+                    <Grid item xs={6}/>
+                    <Grid item xs={4}>
+                        <OutlinedInput
+                            id="search"
+                            label="Search"
+                            type="search"
+                            fullWidth={80}
+                            startAdornment={<InputAdornment position="start"><SearchIcon/></InputAdornment>}
+                            onChange={this.onChangeSearchHandler}
+                        />
+                    </Grid>
+                    <Grid item xs={2}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            size="medium"
+                            className={classes.button}
+                            component={ListItemLink}
+                            to={CREATE_IMPORT_REQUEST_PAGE_PATH}
+                        >
+                            Create IR
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>
+                            <ImportRequestsTable />
+                        </Paper>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                    <Paper className={classes.paper}>
-                        <ImportRequestsTable />
-                    </Paper>
-                </Grid>
-            </Grid>
-        </Container>
+            </Container>
 
-    );
-};
+        );
+    }
+}
+
+export default withStyles(styles)(ImportRequestsContainer);
