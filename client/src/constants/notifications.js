e00053e4ba4a73e0511367c10841e1aa986b952c
Volodymyr Chukhalonok <vchukhalonok@lohika.com>
2020-02-03 12:56:37 +0200

    Error handling

--- client/src/constants/notifications.js
+++ client/src/constants/notifications.js
@@ -1,2 +1,5 @@
 export const DELETE_IMPORT_REQUEST_CONFIRMATION = 'Do you want to delete import request?';
 export const DELETE_OFFER_CONFIRMATION = 'Do you want to delete offer?';
+export const EXPORT_OFFERS_ALERT = 'Error during exporting offers!';
+export const EXPORT_OFFERS_ERROR = 'Something was wrong';
+
--- client/src/pages/offers/containers/OffersContainer.jsx
+++ client/src/pages/offers/containers/OffersContainer.jsx
@@ -1,4 +1,7 @@
-import React, { Component } from "react";
+import React, {
+    Component,
+    Fragment
+} from "react";
 import {
     Container,
     Grid,
@@ -13,6 +16,11 @@ import rest, { DATA_TYPE } from "../../../utils/rest";
 import { METHODS } from "../../../constants/methods";
 import { EXPORT_YANDEX_MARKET_URL } from "../../../constants/urls";
 import config from "../../../config";
+import Alert from "../../../components/alert";
+import {
+    EXPORT_OFFERS_ALERT,
+    EXPORT_OFFERS_ERROR
+} from "../../../constants/notifications";
 
 const styles  = theme => ({
     container: {
@@ -37,7 +45,9 @@ class OffersContainer extends Component {
         this.state = {
             search: '',
             numSelected: 0,
-            selectedItems: []
+            selectedItems: [],
+            openAlert: false,
+            alertErrorMessage: ''
         }
     }
 
@@ -84,20 +94,29 @@ class OffersContainer extends Component {
         const offersIds = this.state.selectedItems;
         const importRequestId = this.props.match.params.importRequestId;
 
-        rest(
-            `${config.backendUrl}${EXPORT_YANDEX_MARKET_URL}`,
-            METHODS.POST,
-            {
-                importRequestId,
-                offersIds
-            },
-            DATA_TYPE.XML
-        ).then((response) => {
-            const blob = new Blob([response], {type: "text/xml;charset=utf-8"});
-            saveAs(blob, `${importRequestId}_export_${moment().format("DD_MM_YYYY_hh_mm")}.yml`);
-        }).catch((error) => {
-            console.log(error);
-        });
+        if(offersIds.length === 0) {
+            this.openExportAlert("Please, select offers!");
+        } else {
+            rest(
+                `${config.backendUrl}${EXPORT_YANDEX_MARKET_URL}`,
+                METHODS.POST,
+                {
+                    importRequestId,
+                    offersIds
+                },
+                DATA_TYPE.XML
+            ).then((response) => {
+                if(response) {
+                    const blob = new Blob([response], {type: "text/xml;charset=utf-8"});
+                    saveAs(blob, `${importRequestId}_export_${moment().format("DD_MM_YYYY_hh_mm")}.yml`);
+                } else {
+                    this.openExportAlert(EXPORT_OFFERS_ERROR);
+                }
+            }).catch((error) => {
+                console.log(error);
+                this.openExportAlert(error);
+            });
+        }
     };
 
     exportAllHandler = () => {
@@ -111,11 +130,31 @@ class OffersContainer extends Component {
             },
             DATA_TYPE.XML
         ).then((response) => {
-            const blob = new Blob([response], {type: "text/xml;charset=utf-8"});
-            saveAs(blob, `${importRequestId}_export_all_${moment().format("DD_MM_YYYY_hh_mm")}.yml`);
+            if(response) {
+                const blob = new Blob([response], {type: "text/xml;charset=utf-8"});
+                saveAs(blob, `${importRequestId}_export_all_${moment().format("DD_MM_YYYY_hh_mm")}.yml`);
+            } else {
+                this.openExportAlert(EXPORT_OFFERS_ERROR);
+            }
         }).catch((error) => {
             console.log(error);
+            this.openExportAlert(error);
         });
+    };
+
+
+    openExportAlert = (error) => {
+        const state = {
+            openAlert: true,
+            alertErrorMessage: error
+        };
+
+        this.setState(state);
+    };
+
+
+    closeExportAlertHandler = () => {
+        this.setState({openAlert: false});
     };
 
     render() {
@@ -123,48 +162,56 @@ class OffersContainer extends Component {
         const { numSelected, selectedItems } = this.state;
 
         return (
-            <Container maxWidth="lg" className={classes.container}>
-                <Grid container spacing={3}>
-                    <Grid item xs={4}/>
-                    <Search onChange={this.onChangeSearchHandler}/>
-                    <Grid item xs={2}>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            size="medium"
-                            className={classes.button}
-                            onClick={this.exportSelectedHandler}
-                            to=''
-                        >
-                            Export Selected
-                        </Button>
+            <Fragment>
+                <Alert
+                    message={`${EXPORT_OFFERS_ALERT} ${this.state.alertErrorMessage}`}
+                    title="Alert"
+                    closeHandler={this.closeExportAlertHandler}
+                    isOpen={this.state.openAlert}
+                />
+                <Container maxWidth="lg" className={classes.container}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={4}/>
+                        <Search onChange={this.onChangeSearchHandler}/>
+                        <Grid item xs={2}>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                size="medium"
+                                className={classes.button}
+                                onClick={this.exportSelectedHandler}
+                                to=''
+                            >
+                                Export Selected
+                            </Button>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                size="medium"
+                                className={classes.button}
+                                onClick={this.exportAllHandler}
+                                to=''
+                            >
+                                Export All
+                            </Button>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Paper className={classes.paper}>
+                                <OffersTable
+                                    importRequestId={this.props.match.params.importRequestId}
+                                    getSearchString={this.getSearchString}
+                                    numSelected={numSelected}
+                                    selectedItems={selectedItems}
+                                    offerCheckBoxHandler={this.offerCheckBoxHandler}
+                                    offerCheckBoxSelectAllHandler={this.offerCheckBoxSelectAllHandler}
+                                />
+                            </Paper>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={2}>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            size="medium"
-                            className={classes.button}
-                            onClick={this.exportAllHandler}
-                            to=''
-                        >
-                            Export All
-                        </Button>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Paper className={classes.paper}>
-                            <OffersTable
-                                importRequestId={this.props.match.params.importRequestId}
-                                getSearchString={this.getSearchString}
-                                numSelected={numSelected}
-                                selectedItems={selectedItems}
-                                offerCheckBoxHandler={this.offerCheckBoxHandler}
-                                offerCheckBoxSelectAllHandler={this.offerCheckBoxSelectAllHandler}
-                            />
-                        </Paper>
-                    </Grid>
-                </Grid>
-            </Container>
+                </Container>
+            </Fragment>
         );
     }
 };
