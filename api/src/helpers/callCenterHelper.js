f3e00fac3e285899dbfe01ee285e3bb374dfd281
Volodymyr Chukhalonok <vchukhalonok@lohika.com>
2020-06-23 17:51:11 +0300

    Modify phone CC processing method

--- api/src/helpers/callCenterHelper.js
+++ api/src/helpers/callCenterHelper.js
@@ -1,8 +1,29 @@
-import { findKey } from "lodash";
+import {
+    findKey,
+    words
+} from "lodash";
 import rest from "../utils/rest";
 import { METHODS } from "../constants/methods";
 import { CALLCENTER_BASE_URL } from "../constants/urls";
 import { CallcenterImportRequestModel } from "../models";
+
+const operatorsCodes = [
+    "039",
+    "067",
+    "068",
+    "096",
+    "097",
+    "098",
+    "050",
+    "066",
+    "095",
+    "099",
+    "063",
+    "093",
+    "091",
+    "092",
+    "094"
+];
 
 export const isOfferExists = async (offerUrl) => {
     const response = await rest(
@@ -47,6 +68,30 @@ const currencyReplacer = currency => {
     });
 }
 
+
+const getOperatorCodePositionIndex = phones => {
+    for(let i = 0; i < operatorsCodes.length; i++) {
+        let codePositionIndex = phones.indexOf(operatorsCodes[i]);
+        if(codePositionIndex > -1) {
+            return codePositionIndex;
+        }
+    }
+    return -1;
+}
+
+const normalizePhones = phones => {
+    let onlyDigitalPhones = phones.replace(/[^+\d]/g, '');
+    const phonesArray = [];
+    let codePositionIndex = getOperatorCodePositionIndex(onlyDigitalPhones);
+    while(codePositionIndex > -1) {
+        const phone = onlyDigitalPhones.slice(codePositionIndex, codePositionIndex + 10);
+        phonesArray.push(phone);
+        onlyDigitalPhones = onlyDigitalPhones.slice(codePositionIndex + 10);
+        codePositionIndex = getOperatorCodePositionIndex(onlyDigitalPhones);
+    }
+    return phonesArray;
+}
+
 const convertToExportBundle = (importRequest, offer) => ({
     name: offer.title,
     url: offer.url,
@@ -57,7 +102,7 @@ const convertToExportBundle = (importRequest, offer) => ({
     city: offer.city,
     params: offer.details.map(detail => ({ name: detail.measure, value: detail.value.join(",") })),
     images: offer.srcImages,
-    phones: [offer.phone.replace(' ', '')],
-    user_name: offer.userName
+    phones: normalizePhones(offer.phone),
+    user_name: offer.userName.trim()
 });
 
